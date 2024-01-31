c3b3f5ab1480ca3a8825ad2fd7d7babd5f7777f7
Kanagapriya-CenTrak <117167640+KanagapriyaJ@users.noreply.github.com>
2024-01-31 19:30:06 +0530

    V163
    1) Updated LF Range values: (Low: 3, Medium: 6, High: 16)
    2) Updated the LF BIT VALUE for Data 2 (175->181), Data 4 (310->318) and Data 6 (444->454)
    3) Changed the Starbit pulse width for Low & Medium to 77 (616 us). No change in Startbit pulse width for LF Range High: 71 (568 us)
    4) Green LED Turned ON for 500ms for a successful handshake.
    5) Removed Proximity Signal Frequency & Modulator Timings configuration from Proximity Sensor Initialization.
    6) Proximity Algorithm Change: 10 fresh measurements after each proximity trigger
    7) Proximity Algorithm Change: Proximity Threshold value to 100. This can be configured through the production tool.
    Note:Please make sure the following fields are configured correctly in the Production Tool Settings.ini file.
        a) TXPower b) Measurement Rate c) Threshold
        [SETTINGS]
        MEASUREMENT_RATE = 1
        THRESHOLD = 100
        IRTX_POWER = 20

--- APPLICATION/DIM/IAR/main.h
+++ APPLICATION/DIM/IAR/main.h
@@ -1,7 +1,7 @@
 #ifndef _MAIN_H
 #define _MAIN_H
 #ifdef __2X__
-#define VERSION                                             9
+#define VERSION                                             163
 #else
 #define VERSION                                             4
 #endif
